e10f7e9b2c7a69fe96b264c4e58255fd2933d344
Andric Tham <andric@nuisance.co>
2018-03-04 17:19:42 +0800

    chore: Fix indentation style

--- reducers/index.js
+++ reducers/index.js
@@ -2,20 +2,20 @@ import { RECEIVE_ENTRIES, ADD_ENTRY } from '../actions'
 
 const entries = (state = {}, action) => {
 	switch (action.type) {
-		case RECEIVE_ENTRIES:
-			return {
-				...state,
-				...action.entries,
-				// Merge new entries with our existing state
-				// https://davidwalsh.name/merge-objects
-			}
-		case ADD_ENTRY:
-			return {
-				...state,
-				...action.entry,
-			}
-		default:
-			return state
+	case RECEIVE_ENTRIES:
+		return {
+			...state,
+			...action.entries,
+			// Merge new entries with our existing state
+			// https://davidwalsh.name/merge-objects
+		}
+	case ADD_ENTRY:
+		return {
+			...state,
+			...action.entry,
+		}
+	default:
+		return state
 	}
 }
 
